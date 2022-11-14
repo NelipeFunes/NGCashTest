d11cbe3ee36e10007c5ea06d87e6fd2594882933
Felipe Nunes <felipedevnunes@gmail.com>
2022-11-14 19:16:17 -0300

    Install depencies and star development

---
+++ BackEnd/src/index.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express from 'express';
+
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.API_PORT || 3010;
+
+app.get('/tests', (req, res) => {
+  res.status(200).send(`Rodando na porta ${PORT}`)
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
