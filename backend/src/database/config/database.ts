30a9fef4c78bc89774e377a794d6b8ee0cb1b8f8
Felipe Nunes <felipedevnunes@gmail.com>
2022-11-15 16:21:59 -0300

    Fix: Docker-compose

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -4,7 +4,7 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '415263',
-  database: process.env.DB || 'ng_db',
+  database: process.env.DB_NAME || 'ng_db',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
   dialect: 'postgres',
