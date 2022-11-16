3f15695b4f1e26f133bfbedbfb8b669a86f3b979
Felipe Nunes <felipedevnunes@gmail.com>
2022-11-16 16:31:13 -0300

    Changes to seeder

--- backend/src/database/seeders/20221115031952-users.js
+++ backend/src/database/seeders/20221115031952-users.js
@@ -2,14 +2,14 @@ module.exports = {
   up: async (queryInterface) => {
     await queryInterface.bulkInsert('users', [
       {
-        username: 'FelipeNunes',       
+        username: 'felipenunes',       
         password: '$2a$10$pY3Qmdl7zNXGMqbB0Mf.CeB3b5WLHs3OhMUC5f4hujXm6/4VcT2AG',
         account_id: 1
           // senha: Felipe415263
       },
 
       {
-        username: 'User',
+        username: 'user',
         password: '$2a$10$9euviNihHi0BDR49tD4T2OVGBVSR5xzFqwEDobfB4lUpHWwOLaAQa', 
         account_id: 2
           // senha: User415263
